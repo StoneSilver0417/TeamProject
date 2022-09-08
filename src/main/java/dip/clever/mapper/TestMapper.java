6e1dd4ac6050967d281db9535be25e39ceaf8a65
astinel <51503141+astinel@users.noreply.github.com>
2022-09-08 20:38:33 +0900

    8일
    카테고리 리스트 출력
    테스트 리스트 출력

--- src/main/java/dip/clever/mapper/TestMapper.java
+++ src/main/java/dip/clever/mapper/TestMapper.java
@@ -5,8 +5,13 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import dip.clever.model.Category;
+import dip.clever.model.Test;
 
 @Mapper
 public interface TestMapper{	
 	public List<Category> selectCategoryList();
+	
+	public List<Test> selectTestList(int categoryNo);
+	
+	public Category selectCategory(int categoryNo);
 }
---
+++ src/main/java/dip/clever/model/Test.java
@@ -0,0 +1,11 @@
+package dip.clever.model;
+
+import lombok.Data;
+
+@Data
+public class Test {
+	private int testNo;
+	private int categoryNo;
+	private String managerId;
+	private String testName;
+}
