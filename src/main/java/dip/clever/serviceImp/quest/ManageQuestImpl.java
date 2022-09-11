2e2505a7c300b38eb24a01a52ec32fc987196a63
WS-SHIM <110947438+WS-SHIM@users.noreply.github.com>
2022-09-11 11:33:48 +0900

    문제관리 구현체 추가

---
+++ src/main/java/dip/clever/serviceImp/quest/ManageQuestImpl.java
@@ -0,0 +1,53 @@
+package dip.clever.serviceImp.quest;
+
+import dip.clever.dto.QuestInfoDto;
+import dip.clever.dto.RoundInfoDto;
+import dip.clever.dto.TestInfoDto;
+import dip.clever.service.quest.MangeQuest;
+
+public class ManageQuestImpl implements MangeQuest {
+    @Override
+    public void addRound(RoundInfoDto roundInfoDto) {
+
+    }
+
+    @Override
+    public void modifyRound(long roundNo, RoundInfoDto roundInfoDto) {
+
+    }
+
+    @Override
+    public void deleteRound(long roundNo) {
+
+    }
+
+    @Override
+    public void addTest(TestInfoDto testInfoDto) {
+
+    }
+
+    @Override
+    public void modifyTest(long testNo, TestInfoDto testInfoDto) {
+
+    }
+
+    @Override
+    public void deleteTest(long testNo) {
+
+    }
+
+    @Override
+    public void addQuest(QuestInfoDto questInfoDto) {
+
+    }
+
+    @Override
+    public void modifyQuest(long questNo, QuestInfoDto questInfoDto) {
+
+    }
+
+    @Override
+    public void deleteQuest(long questNo) {
+
+    }
+}
