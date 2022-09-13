86ce6025f23e698a9b8b585005dfd9347f3ad3bb
astinel <51503141+astinel@users.noreply.github.com>
2022-09-13 16:28:03 +0900

    13일
    프로필 사진 변경 구현

--- src/main/java/dip/clever/controller/UserController.java
+++ src/main/java/dip/clever/controller/UserController.java
@@ -1,11 +1,21 @@
 package dip.clever.controller;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import dip.clever.model.User;
 import dip.clever.service.UserService;
 
 @Controller
@@ -61,5 +71,58 @@ public class UserController {
 	public String leave() {
 		return "edit_forms/edit-email";
 	}
+	
+	@PostMapping("/user/uploadTemp")
+	public ResponseEntity<Boolean> uploadTemp(HttpServletRequest httpServletRequest, @RequestParam("profileImage") MultipartFile file){
+		User user = (User) httpServletRequest.getSession().getAttribute("user");		
+		String path = "C:\\Users\\8\\Documents\\GitHub\\TeamProject\\src\\main\\resources\\static\\imgs\\profile\\temp\\";	
+		
+		if (user == null || file.getSize() == 0)
+			return new ResponseEntity<Boolean> (false, HttpStatus.OK);
 
+		System.out.println("파일 이름 : " + file.getOriginalFilename());
+		System.out.println("파일 크기 : " + file.getSize());
+
+		path += user.getUserId() + ".png";
+		System.out.println(path);
+		try (	FileOutputStream fos = new FileOutputStream(path); 
+				InputStream is = file.getInputStream();) {
+			int readCount = 0;
+			byte[] buffer = new byte[1024];
+			while ((readCount = is.read(buffer)) != -1) {
+				fos.write(buffer, 0, readCount);
+			}
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			throw new RuntimeException("file Save Error");
+		}
+		return new ResponseEntity<Boolean> (true, HttpStatus.OK);
+	}
+	
+	@PostMapping("/user/uploadProfile")
+	public ResponseEntity<Boolean> uploadProfile(HttpServletRequest httpServletRequest, @RequestParam("profileImage") MultipartFile file){
+		User user = (User) httpServletRequest.getSession().getAttribute("user");		
+		String path = "C:\\Users\\8\\Documents\\GitHub\\TeamProject\\src\\main\\resources\\static\\imgs\\profile\\user\\";	
+		
+		if (user == null || file.getSize() == 0)
+			return new ResponseEntity<Boolean> (false, HttpStatus.OK);
+
+		System.out.println("파일 이름 : " + file.getOriginalFilename());
+		System.out.println("파일 크기 : " + file.getSize());
+
+		path += user.getUserId() + ".png";
+		System.out.println(path);
+		try (	FileOutputStream fos = new FileOutputStream(path); 
+				InputStream is = file.getInputStream();) {
+			int readCount = 0;
+			byte[] buffer = new byte[1024];
+			while ((readCount = is.read(buffer)) != -1) {
+				fos.write(buffer, 0, readCount);
+			}
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			throw new RuntimeException("file Save Error");
+		}
+		return new ResponseEntity<Boolean> (true, HttpStatus.OK);
+	}
 }
