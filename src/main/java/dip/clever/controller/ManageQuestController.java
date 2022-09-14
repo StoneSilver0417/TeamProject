7560b98134fa8c4b1cb4b71fa16cb0cb1b14a81d
ShimWooSeok <waterdrop11@naver.com>
2022-09-14 10:57:49 +0900

    문제등록

--- src/main/java/dip/clever/controller/ManageQuestController.java
+++ src/main/java/dip/clever/controller/ManageQuestController.java
@@ -3,17 +3,12 @@ package dip.clever.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import dip.clever.model.Choice;
 import dip.clever.model.Quest;
 import dip.clever.model.Round;
-import dip.clever.model.User;
 import dip.clever.service.MangeQuestService;
-import dip.clever.service.UserService;
-import lombok.RequiredArgsConstructor;
 
 
 
@@ -26,13 +21,14 @@ public class ManageQuestController {
 	
 	// 문제등록
 	@PostMapping("/insertQuest")
-	public String insertQuest(Model model, int no, Choice choice, Quest quest) {
+	public String insertQuest(Model model, int roundNo, Choice choice, Quest quest) {
 		Round round = new Round();
-		round.setRoundNo(no);
+		round.setRoundNo(roundNo);
 		model.addAttribute("round", mangeQuestService.selectRound(round));
-		quest.setRoundNo(no);
-		mangeQuestService.insertQuest(quest);
+		quest.setRoundNo(roundNo);
 		mangeQuestService.insertChoice(choice);
+		quest.setChoiceNo(mangeQuestService.selectChoiceNo());
+		mangeQuestService.insertQuest(quest);
 		return "questForm";
 	}
 	
