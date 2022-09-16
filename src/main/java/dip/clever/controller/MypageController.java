e30c7c58234c685ff124956fc0f8c0e99e922e45
Adhnoow <104211706+adhnoow@users.noreply.github.com>
2022-09-16 18:47:33 +0900

    마이페이지 수정
    마이페이지 수정

--- src/main/java/dip/clever/controller/MypageController.java
+++ src/main/java/dip/clever/controller/MypageController.java
@@ -1,21 +1,49 @@
 package dip.clever.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("mypage")
 public class MypageController {
 
+	// ** mypage **
 	// mypage 반환
-	@RequestMapping("/mypage")
+	@GetMapping("")
 	public String mypage() {
 		return "mypage/mypage";
 	}
 
+	// 내가 푼 문제 조회
+	@PostMapping("/quests")
+	public String mySolvedQuest() {
+		return "mypage/activity/mypage-activity";
+	}
+
+	// 내가 올린 문제 조회
+	@PostMapping("/upload-quests")
+	public String myQuest() {
+		return "mypage/activity/mypage-quest";
+	}
+
+	// 내 댓글 조회
+	@PostMapping("/comments")
+	public String myReply() {
+		return "mypage/activity/mypage-reply";
+	}
+
+	// 레벨 기준 조회
+	@PostMapping("/level")
+	public String myLevel() {
+		return "mypage/activity/mypage-level";
+	}
+
+	// mypage settings
 	// mypage - 개인정보 수정
-	@RequestMapping("/mypage-setting")
+	@PostMapping("/mypage-setting")
 	public String mypageSetting() {
 		return "mypage/settings/mypage-setting";
 	}
